508206112340c9d604da2235f5d43399a04395ec
WhosWiFi <WhosWiFi@gmail.com>
2024-09-08 14:42:06 -0400

    Static home page and a puzzle added

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+
 app.get('/', function (req, res) {
-  data = {Danger:"Close", Hacker:"Here"};
-  res.send(data['Danger']);
+  fs.readFile('main.html', function (err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
 });
+
 app.get('/hacker', function (req, res) {
   data = {Danger:"Close", Hacker:"You are a hacker"};
   res.send(data['Hacker']);
 });
+
 app.get('/admin', function (req, res) {
   data = {Danger:"Close", Hacker:"Here"};
   is_admin = False;
   res.send(is_admin);
 });
+
+app.get('/hacker/secret.txt', function (req, res) {
+  fs.readFile('secret.txt', function (err, data) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(data);
+    return res.end();
+  });
+});
+
 app.get('/robots.txt', function (req, res) {
   fs.readFile('robots.txt', function (err, data) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -21,6 +36,7 @@ app.get('/robots.txt', function (req, res) {
     return res.end();
   });
 });
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
