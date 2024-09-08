ece18fa48a1ad20e403aabae95cf091cbe438c41
WhosWiFi <WhosWiFi@gmail.com>
2024-09-08 17:01:47 -0400

    Working input to post request

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var bodyParser = require('body-parser');
+
+// Middleware
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   fs.readFile('main.html', function (err, data) {
@@ -12,13 +17,12 @@ app.get('/', function (req, res) {
 
 app.get('/hacker', function (req, res) {
   data = {Danger:"Close", Hacker:"You are a hacker"};
-  res.send(data['Hacker']);
+  return res.send(data['Hacker']);
 });
 
 app.get('/admin', function (req, res) {
-  data = {Danger:"Close", Hacker:"Here"};
-  is_admin = False;
-  res.send(is_admin);
+  data = {Admin:"Welcome to the admin dashboard"}
+  return res.send(data['Admin']);
 });
 
 app.get('/hacker/secret.txt', function (req, res) {
@@ -37,6 +41,18 @@ app.get('/images/background.jpeg', function (req, res) {
   });
 });
 
+app.post('/validate_flag', function (req, res) {
+  const { flag } = req.body;
+
+  if (flag == 'WiFi{y0U_kN0w_fuZZ1Ng!}') {
+      return res.redirect('/admin');
+  } else {
+      res.writeHead(403, {'Content-Type': 'application/json'})
+      res.write("Flag is incorrect.")
+      return res.end(); // Send 418 I'm a teapot if it doesn't match
+  }
+});
+
 app.get('/robots.txt', function (req, res) {
   fs.readFile('robots.txt', function (err, data) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -46,5 +62,5 @@ app.get('/robots.txt', function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('DangerClose listening on port 3000!');
 });
